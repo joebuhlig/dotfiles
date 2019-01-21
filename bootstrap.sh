92f22fec285eff7dd7c12214165fe88e37dc93b9
Joe Buhlig <joe@joebuhlig.com>
2019-01-21 22:29:14 +0000

    Sync folder to /root

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,22 @@
 git -C $HOME/dotfiles pull origin master
 
+rsync --exclude ".git/" \
+	--exclude ".DS_Store" \
+	--exclude ".osx" \
+	--exclude "bootstrap.sh" \
+	--exclude "README.md" \
+	--exclude "LICENSE-MIT.txt" \
+	-avh --no-perms . ~;
+
+source ~/.bash_profile;
+
 # install vim setup
 echo 'setting up vim'
 if [ -d "$HOME/.vim" ]
 then
 	git -C $HOME/.vim pull origin master
 else
-	git clone --recurse-submodules https://github.com/joebuhlig/vim.git $HOME/.vim
+	git clone --recurse-submodules git@github.com/joebuhlig/vim.git $HOME/.vim
 fi
 
 if [ ! -L $HOME/.vimrc ]
@@ -18,3 +28,15 @@ fi
 echo 'installing plugins for vim'
 vim +PluginClean +qall
 vim +PluginInstall +qall
+
+# install todo setup
+
+echo 'setting up todo'
+if [ -d "$HOME/.todo" ]
+then
+	git -C $HOME/.todo pull origin master
+else
+	git clone git@github.com:joebuhlig/todo.git $HOME/.todo
+fi
+
+
